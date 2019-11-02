00686a8a43c45de88951409dd1f35d0e69474373
Paulo <paulo.zti@gmail.com>
2019-11-02 09:57:50 -0300

    Created enemy explosion animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
+using System.Collections;
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float _MoveSpeed = 5;
     private Player _player;
+    private Animator _destroyAnimation;
+    private bool _exploding = false;
     private void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
+        _destroyAnimation = gameObject.GetComponent<Animator>();
     }
     void Update()
     {
         transform.Translate(Vector3.down * _MoveSpeed * Time.deltaTime);
-        if (transform.position.y < -5.6f)
+        if (transform.position.y < -5.6f && !_exploding)
         {
             float newX = Random.Range(-5.5f, 8.5f);
             transform.position = new Vector3(newX, 8f, 0);
@@ -20,11 +24,13 @@ public class Enemy : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == ("Player"))
+        if (other.tag == ("Player") && !_exploding)
         {
             if (_player != null)
             {
+                
                 _player.Damage();
+                
             }
             Destroy(this.gameObject);
         }
@@ -34,8 +40,21 @@ public class Enemy : MonoBehaviour
             if (_player != null)
             {
                 _player.UpdateScore(10);
+                _destroyAnimation.SetTrigger("OnEnemyDeath");
+                _exploding = true;
             }
-            Destroy(this.gameObject);
+            StartCoroutine(OnDeath());
         }
+    }
+
+    IEnumerator OnDeath()
+    {
+        while (_exploding)
+        {
+            yield return new WaitForSeconds(3f);
+            Destroy(this.gameObject);
+            _exploding = false;
+        }
+        
     }
 }
