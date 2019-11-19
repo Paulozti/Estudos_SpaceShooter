26fae384a045d0d8c83dcbb517948d0dac943a53
Paulo <paulo.zti@gmail.com>
2019-11-19 18:42:55 -0300

    AdicionadoAlgunsSoms

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -38,8 +38,13 @@ public class Player : MonoBehaviour
     private SpawnManager _spawner;
     private UImanager _uiManager;
 
+    [SerializeField]
+    private AudioClip _shootLaserSound;
+    private AudioSource _audioSource;
+
     void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
         transform.position = new Vector3(0, 0, 0);
         _spawner = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
 
@@ -53,6 +58,11 @@ public class Player : MonoBehaviour
         {
             Debug.LogError("UI Manager is NULL.");
         }
+
+        if(_audioSource == null)
+        {
+            Debug.LogError("Audio Source on player is NULL.");
+        }
     }
 
     
@@ -64,7 +74,7 @@ public class Player : MonoBehaviour
         {
             FireLaser();
         }
-    }
+    }   
 
     void CalculateMoviment() 
     {
@@ -112,6 +122,8 @@ public class Player : MonoBehaviour
         {
             newLaser.transform.parent = _LaserContainer.transform;
         }
+        _audioSource.clip = _shootLaserSound;
+        _audioSource.Play();
      }
 
     public void Damage()
